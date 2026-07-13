f3ffa2c7c55589c234e45ee8531d5ba891e5bfe8
phuriyut007-dotcom <phuriyut007@gmail.com>
2026-07-13 10:53:28 +0700

    Update App .js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let r='Reset'
+  let tn='React Native'
+  const el=(
+      <div>
+        Create Web & Mobile Apps<br/>
+        with {r} & {tn}
+      </div>
+    )
+  return el  
 }
 
 export default App;
