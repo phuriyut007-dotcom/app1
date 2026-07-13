324ca8388fcfea97b309e7b6d17a636431a12513
phuriyut007-dotcom <phuriyut007@gmail.com>
2026-07-13 11:14:19 +0700

    Update App .js 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import "./style.css";
 
 function App() {
-    const divStyle={
-      color:'red',
-      backgroundColor:'powderblue',
-      fontSize:'larger',
-      padding:'3px'
-    }
-  return <div style={divStyle}>Hello React</div>
+  return (
+    <>
+      <h2>DeveloperThai.com</h2>
+      <div className="title">React & React Native</div>
+    </>
+  );
 }
 
 export default App;
