0e88e62527afbf8b896d73ebed362407918c3885
Your Name <phuriyut007@gmail.com>
2026-07-20 10:46:48 +0700

    function components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./style.css";
+import React from "react";
+import { Content, Footer, Header } from "./func-components";
 
 function App() {
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className="title">React & React Native</div>
+      <Header/>
+      <Content/>
+      <Footer/>
     </>
   );
 }
 
 export default App;
+
